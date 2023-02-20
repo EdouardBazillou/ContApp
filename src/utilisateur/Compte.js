c1e3c527faec3d1b120f510ca188c7f073e19a47
EdouardBazillou <edouard.bazillou@gmail.com>
2023-02-20 15:37:26 +0100

    form commit

--- src/utilisateur/Compte.js
+++ src/utilisateur/Compte.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 function compte() {
   return (
     <div>
-      <h2>Compte</h2>
+      <h1>Compte</h1>
       <nav>
         <Link to="/">Se Connecter</Link>
         <Link to="/filActu">Mon Fil Actu</Link>
--- src/utilisateur/login.js
+++ src/utilisateur/login.js
@@ -1,9 +1,85 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 function Login() {
+  const [user, setUser] = useState("");
+  const [userList, setUserlist] = useState([]);
+
+  const handleInput = (e) => {
+    setUser({
+      [e.target.name]: e.target.value,
+      [e.target.firstName]: e.target.value,
+      [e.target.pseudo]: e.target.value,
+      [e.target.mail]: e.target.value,
+      [e.target.phone]: e.target.value,
+      [e.target.motdepasse]: e.target.value,
+    });
+  };
+
+  const addUser = (e) => {
+    e.preventDefault();
+    setUserlist([...userList, user]);
+    console.log("new liste", userList);
+  };
+
+  useEffect(() => {
+    // console.log("user", user);
+  }, [user]);
+
+  useEffect(() => {
+    // console.log("userList", userList);
+  }, [userList]);
+
   return (
     <div>
       <h1> Bienvenu sur ContAPP ! </h1>
-      <h2>Veuillez Vous Connectez</h2>
+      <h2>Veuillez Créer Votre compte</h2>
+      <form id="form">
+        <input
+          type="text"
+          className="inputName"
+          placeholder="Votre nom"
+          name="name"
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          className="inputFirstName"
+          placeholder="Votre Prénom"
+          name="firstName"
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          className="pseudo"
+          placeholder="Votre Pseudo"
+          name="pseudo"
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          className="inputMail"
+          placeholder="Votre Mail"
+          name="mail"
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          className="inputPhone"
+          placeholder="Votre Téléphone"
+          name="phone"
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          className="motdepasse"
+          placeholder="Votre Mot De Passe"
+          name="motdepasse"
+          onChange={handleInput}
+        />
+        <button type="submit" onClic={addUser}>
+          S'inscire
+        </button>
+      </form>
       <nav>
         <Link to="/compte">Mon Compte</Link>
         <Link to="/filActu">Mon Fil Actu</Link>
