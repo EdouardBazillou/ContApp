cd638e2823d5457d5ecb7fd9b34a4da8a28ea5b9
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-02-21 16:33:06 +0100

    Merge branch 'main' of https://github.com/EdouardBazillou/ContApp

--- src/utilisateur/login.js
+++ src/utilisateur/login.js
@@ -7,7 +7,38 @@ function Login() {
           <Link to="/filActu">Mon Fil Actu</Link>
         </nav>
       </header>
-      <h1>Je Me Connecte</h1>
+      <div className="containerLogin">
+        <div class="screen">
+          <div class="screenContent">
+            <form class="loginForm">
+              <div className="dataLoginEmail">
+                <input
+                  type="text"
+                  class="loginEmailInput"
+                  placeholder="email"
+                  name="email"
+                />
+              </div>
+              <div className="dataPasswordEmail">
+                <input
+                  type="password"
+                  class="loginPasswordInput"
+                  placeholder="mot de passe"
+                  name="password"
+                />
+              </div>
+              <div>
+                <button
+                  className="buttonLoginSubmit"
+                  placeholder="Se connecter"
+                >
+                  <span className="buttonLoginText">Se connecter</span>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
