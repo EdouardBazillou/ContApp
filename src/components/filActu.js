37f18ba575c2bbb389cfebc1960e733761c0cabc
EdouardBazillou <edouard.bazillou@gmail.com>
2023-02-24 12:13:49 +0100

    filactuupdate

--- src/components/filActu.js
+++ src/components/filActu.js
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useActionData } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Menu from "../Assets/Menu";
 import Footer from "../Assets/Footer";
 
 function Actu() {
   //Fetch GET POST
+
+  const [post, setPost] = useState([]);
 
   const getPost = async () => {
     const options = {
@@ -14,15 +16,37 @@ function Actu() {
       },
     };
     let response = await fetch(
-      `https://social-network-api.osc-fr1.scalingo.io/contapp/posts`,
+      `https://social-network-api.osc-fr1.scalingo.io/contapp/posts?page=1&limit=10`,
       options
     );
     let data = await response.json();
     console.log("data", data);
-    setNewPost(data.posts, data.title);
 
     console.log("je marche");
+    setPost(data.posts);
   };
+
+  const renderActu = () => {
+    if (post.length >= 0) {
+      return post.map((item, index) => {
+        return (
+          <form key={index} className="postContainer">
+            <div>
+              <h1 className="title">{item.title}</h1>
+              <p>{item.content}</p>
+              <p>{item.date}</p>
+              <p>{item.comment}</p>
+              <p>{item.like}</p>
+            </div>
+          </form>
+        );
+      });
+    }
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
 
   return (
     <div>
@@ -30,6 +54,8 @@ function Actu() {
         <Menu />
       </header>
       <h2>Fil Actu</h2>
+      <button onClick={getPost}>Actualiser mon fil</button>
+      <div>{renderActu()}</div>
       <Footer />
     </div>
   );
