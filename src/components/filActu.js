eb282ae82c2206462d83a623f78218a1701704d8
EdouardBazillou <edouard.bazillou@gmail.com>
2023-02-24 15:15:14 +0100

    likecomit

--- src/components/filActu.js
+++ src/components/filActu.js
@@ -30,18 +30,18 @@ function Actu() {
     if (post.length >= 0) {
       return post.map((item, index) => {
         return (
-          <form key={index} className="postContainer">
-            <div>
-              <h1 className="title">{item.title}</h1>
-              <p>{item.content}</p>
-              <p>{item.date}</p>
-              <p>{item.comment}</p>
-              <p>{item.like}</p>
-              <button>aimer</button>
-              <textarea>saisissez Commentaires</textarea>
-              <button onClick={addComment}>commenter</button>
-            </div>
-          </form>
+          <div key={index} className="postContainer">
+            <form>
+              <div>
+                <h1 className="title">{item.title}</h1>
+                <p>{item.content}</p>
+                <p>{item.date}</p>
+                <p>{item.comment}</p>
+                <p>{item.like}</p>
+              </div>
+            </form>
+            <button onClick={postLike}>aimer</button>
+          </div>
         );
       });
     }
@@ -51,37 +51,65 @@ function Actu() {
     getPost();
   }, []);
 
-  const [comments, setComments] = useState([]);
-  const [typeComment, setTypeComment] = useState([]);
+  const [like, setLike] = useState(null);
 
-  // Commentaires
-  const addComment = async (e) => {
-    e.preventDefault();
-
+  const postLike = async () => {
     const options = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "bearer " + localStorage.getItem("@userToken"),
+        Authorization: "bearer" + localStorage.getItem("@userToken"),
       },
-
       body: JSON.stringify({
-        postId: localStorage.getItem("id", post._id),
-        content: { setComments },
+        postId: localStorage.getItem(post.id),
       }),
     };
-
-    //Changement de syntaxe
-    console.log(options);
-    await fetch(
-      `https://social-network-api.osc-fr1.scalingo.io/contapp/post/comment`,
+    let response = await fetch(
+      `https://social-network-api.osc-fr1.scalingo.io/contapp/post/like`,
       options
-    ).then((response) => {
-      return response.json();
-    });
+    );
+    let data = await response.json();
+    console.log("data", data);
 
-    setTypeComment(comments);
+    console.log("je marche");
+    alert("vous avez liker le post ! ");
   };
+
+  // const [comment, setComment] = useState([]);
+
+  // const handleInput = (e) => {
+  //   setComment({ [e.target.name]: e.target.value });
+  //   setUser({ ...comment, [e.target.name]: e.target.value });
+  // };
+
+  // // Commentaires
+  // const addComment = async (e) => {
+  //   e.preventDefault();
+
+  //   const options = {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: "bearer " + localStorage.getItem("@userToken"),
+  //     },
+
+  //     body: JSON.stringify({
+  //       postId: localStorage.getItem("id", post._id),
+  //       content: { setComments },
+  //     }),
+  //   };
+
+  //   //Changement de syntaxe
+  //   console.log(options);
+  //   await fetch(
+  //     `https://social-network-api.osc-fr1.scalingo.io/contapp/post/comment`,
+  //     options
+  //   ).then((response) => {
+  //     return response.json();
+  //   });
+
+  //   setTypeComment(comments);
+  // };
 
   return (
     <div>
