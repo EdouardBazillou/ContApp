003c64a2d413bc403fdf8ea4963ae40bb169a2c7
EdouardBazillou <edouard.bazillou@gmail.com>
2023-02-24 09:13:09 +0100

    filactucommit

--- src/components/filActu.js
+++ src/components/filActu.js
@@ -5,6 +5,25 @@ import Footer from "../Assets/Footer";
 
 function Actu() {
   //Fetch GET POST
+
+  const getPost = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    let response = await fetch(
+      `https://social-network-api.osc-fr1.scalingo.io/contapp/posts`,
+      options
+    );
+    let data = await response.json();
+    console.log("data", data);
+    setNewPost(data.posts, data.title);
+
+    console.log("je marche");
+  };
+
   return (
     <div>
       <header>
